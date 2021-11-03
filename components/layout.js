0e0d255a7186b9c7feaa74788920859a8b88a37e
Jason Yavorska <jason@metaluna.io>
2021-11-03 10:14:56 +0200

    Add required bootstrap charset

--- components/layout.js
+++ components/layout.js
@@ -5,6 +5,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
+        <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
