30e6faee25c778f9695f9dab5396872ba4de6885
Jason Yavorska <jason@metaluna.io>
2021-11-03 13:33:21 +0200

    Add hook for custom styling

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from "../components/layout";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/bootstrap-docs.css";
+import "../styles/custom.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
