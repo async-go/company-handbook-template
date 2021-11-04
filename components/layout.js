103c359fe593c74f36e3c1bdaafffd805183f014
Jason Yavorska <jason@metaluna.io>
2021-11-04 09:47:39 +0200

    Add table of contents

--- components/layout.js
+++ components/layout.js
@@ -14,16 +14,7 @@ export default function Layout({ children }) {
       </header>
 
       <div className="container">
-        <div className="row flex-xl-nowrap">
-          <main className="col-xl-10 bd-content">{children}</main>
-
-          <div className="d-none d-xl-block col-xl-2 bd-toc">
-            <ul className="nav flex-column">
-              <h6 className="nav-item">Table of Contents</h6>
-              <li className="toc-entry toc-h2">Not Implemented</li>
-            </ul>
-          </div>
-        </div>
+          <main>{children}</main>
       </div>
     </>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
 import withMDX from '@next/mdx'
+import slug from 'rehype-slug'
+import toc from 'rehype-toc'
 
 const mdx = withMDX({
   extension: /\.(md|mdx)$/,
   options: {
-    rehypePlugins: [],
+    rehypePlugins: [
+      [slug],
+      [toc, {
+        headings: ['h2', 'h3'],
+        position: "afterbegin",
+        cssClasses: {
+          toc: '',
+        }
+      }],
+    ],
     remarkPlugins: [],
   },
 })
