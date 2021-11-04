b0df09f848e4c05191e40f06106bbe32691e7cd8
Jason Yavorska <jason@metaluna.io>
2021-11-04 09:30:09 +0200

    Remove bootstrap css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Layout from "../components/layout";
 
 import "bootstrap/dist/css/bootstrap.css";
-import "../styles/bootstrap-docs.css";
 import "../styles/custom.css";
 
 export default function MyApp({ Component, pageProps }) {
