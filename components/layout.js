6ed9bb4d45d59e1a89a33434bd4b7a54ebde2bfc
Jason Yavorska <jason@metaluna.io>
2021-11-04 09:30:46 +0200

    Rework layout

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Navbar from "./navbar";
-import TableOfContents from "./tableofcontents";
 
 export default function Layout({ children }) {
   return (
@@ -10,15 +9,21 @@ export default function Layout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <Navbar />
+      <header>
+        <Navbar />
+      </header>
 
-      <div className="container">
-        <main className="bd-main">
-          <div className="bd-content">{children}</div>
-          <div className="bd-toc">
-            <TableOfContents />
+      <div className="container-fluid">
+        <div className="row flex-xl-nowrap">
+          <main className="col-xl-10 bd-content">{children}</main>
+
+          <div className="d-none d-xl-block col-xl-2 bd-toc">
+            <ul className="nav flex-column">
+              <h6 className="nav-item">Table of Contents</h6>
+              <li className="toc-entry toc-h2">Not Implemented</li>
+            </ul>
           </div>
-        </main>
+        </div>
       </div>
     </>
   );
