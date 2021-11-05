602156a3d5312b0b6a79afccfbb5279b23d1ce9b
Jason Yavorska <jason@metaluna.io>
2021-11-05 15:35:05 +0200

    Make body bg-light

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <>
       <header>
-        <nav className="navbar navbar-light bg-light mb-3">
+        <nav className="navbar navbar-light bg-white mb-3">
           <div className="container-fluid">
             <a className="navbar-brand" href="/">
               Home
---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    const pageProps = this.props?.__NEXT_DATA__?.props?.pageProps;
+    return (
+      <>
+        <Html>
+          <Head />
+          <body className="bg-light">
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      </>
+    );
+  }
+}
